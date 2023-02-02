6cffc32dd51ab8e07bed42450688b5ee9bfb7f8f
Mustafa Qazi <mus.qazi999@gmail.com>
2023-02-02 19:23:14 +0530

    added tar gz url

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 
 setup(
     name='example_publish_pypi_medium',
@@ -10,11 +9,21 @@ setup(
     packages=['cygnusdatautils'],
     description='Data utils for cygnus',
     url='https://github.com/CygnusAIInternal/cygnusdatautils',
+    download_url='https://github.com/CygnusAIInternal/cygnusdatautils/archive/refs/tags/v_01.tar.gz',
     keywords='cygnus data utils',
     install_requires=[
             'pandas',
             'numpy',
             'psycopg2-binary'
       ],
-
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
 )
